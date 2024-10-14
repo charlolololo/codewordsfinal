a73e6575b7bbb642d5d92fbdf1156fa741649516
Charlotte Cheng Xi Le <s3989470@student.rmit.edu.au>
2024-10-14 14:10:49 +1100

    Updated glitches
    word for "think" now produces glitches
    
    word for "feel" clears glitches

--- a2_final_collated/glitch_2/glitch_2.js
+++
@@ -1,7 +0,0 @@
-// https://www.youtube.com/watch?v=S1TQCi9axzg&ab_channel=TheCodingTrain (Guest Tutorial #4: Matrix Digital Rain in p5.js with Emily Xie)
-
-function preload() {
-  alarm = loadSound ('data/pixabay_paft_drunk_ring_the_alarms-206689.mp3')
-}
-
-function setup 
